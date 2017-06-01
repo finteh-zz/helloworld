d8a38450893bf38aae0d3cfd5d263d27e382a2b6
rebirther <root@rebirther.ru>
2017-06-02 01:19:51 +0700

    three commit

--- src/main/java/ru/cft/test/HelloWorld.java
+++ src/main/java/ru/cft/test/HelloWorld.java
@@ -1,9 +1,10 @@
 package ru.cft.test;
 
 public class HelloWorld {
-	private static final String DefaultString = "Hello World!";
+	private static final String DefaultString = "Hello World! Let's go!";
 	public static void main(String[] args) {
 		String data = (args.length > 0)? args[0] : DefaultString;
 		System.out.println(data);
+		// test
 	}
 } 
